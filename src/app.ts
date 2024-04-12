0a2fd5ca51af02e0fdaa65d70c5bd11fd57d8266
Shivamani-ragipani <shivamaniragipani@gmail.com>
2024-04-13 01:22:31 +0530

    setup express server

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+
+//Routes
+
+app.get('/', (req, res, next)=> {
+
+    res.json({message : "welcome to elib API's"});
+
+});
+
+export default app;
